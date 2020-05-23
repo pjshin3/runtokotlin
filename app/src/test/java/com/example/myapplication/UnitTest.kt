93c439eeb814fd6e60fa55861b763dadf646f1b2
pjshin3 <tlsvlfwp1@naver.com>
2020-05-23 11:56:44 +0900

    test -test

--- app/src/test/java/com/example/myapplication/UnitTest.kt
+++ app/src/test/java/com/example/myapplication/UnitTest.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.os.Build
 import kotlinx.android.synthetic.main.main.*
 import org.junit.Assert.assertEquals
 import org.junit.Rule
@@ -7,9 +8,11 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [Build.VERSION_CODES.P])
 class UnitTest {
     @Test
     fun `textview test`(){
